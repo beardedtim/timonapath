029e51955993b868d958861f90067e8fa7eacd16
Tim Roberts <timiroberts@gmail.com>
2021-11-12 13:05:36 -0500

    Update Dynamic Creation

--- scripts/parse-markdown-into-html.js
+++ scripts/parse-markdown-into-html.js
@@ -1,60 +1,80 @@
-const { resolve } = require('path')
-const fs = require('fs/promises')
-const read = require('./get-files-of-dir-recursively')
-const transform = require('../transformers/markdown-to-html')
-const createSnippetPage = require('../transformers/snippets-into-html')
-const createArticlesPage = require('../transformers/articles-into-html')
+const { resolve } = require("path");
+const fs = require("fs/promises");
+const read = require("./get-files-of-dir-recursively");
+const transform = require("../transformers/markdown-to-html");
+const createSnippetPage = require("../transformers/snippets-into-html");
+const createArticlesPage = require("../transformers/articles-into-html");
 
-const rootDir = resolve(__dirname, '..')
-const dataDir = resolve(rootDir, 'data')
-const outputDir = resolve(rootDir, 'app')
+const rootDir = resolve(__dirname, "..");
+const dataDir = resolve(rootDir, "data");
+const outputDir = resolve(rootDir, "app");
 
-const inputFileType = 'md'
-const outputFileType = 'html'
+const inputFileType = "md";
+const outputFileType = "html";
 
 const landingPageTemplates = {
   snippets: createSnippetPage,
-  articles: createArticlesPage
-}
+  articles: createArticlesPage,
+};
 
 const main = async () => {
-  const iterator = await read(dataDir, inputFileType)
-  const types = {}
+  const iterator = await read(dataDir, inputFileType);
+  const types = {};
 
   for await (const str of iterator) {
     // slice 1 because it starts with /
-    const inputPath = str.replace(rootDir, '').replace('data', '').slice(1)
+    const inputPath = str.replace(rootDir, "").replace("data", "").slice(1);
     // slice 0 -input.length because we wnt to remove the file type
-    const outputPath = outputDir + inputPath.slice(0, -inputFileType.length) + outputFileType
+    const outputPath =
+      outputDir + inputPath.slice(0, -inputFileType.length) + outputFileType;
     // slice 0 -output.length - 1 because we want to not have the file extension
-    const url = outputPath.replace(outputDir, '').slice(0, -outputFileType.length - 1)
+    const url = outputPath
+      .replace(outputDir, "")
+      .slice(0, -outputFileType.length - 1);
 
-
-    const { template, metadata } = await transform(str)
-    await fs.writeFile(outputPath, template)
-
+    const split = inputPath.split("/");
     // _ because the first value is empty due to /
     // starting the string
-    const [_, type] = inputPath.split('/')
+    split.shift()
+    // because we don't care about the last value
+    split.pop()
+    const [type] = split
+    const folderPath = split.join('/')
+    try {
+      await fs.mkdir(`${outputDir}/${folderPath}`, { recursive: true });
+    } catch (e) {
+      console.error(e);
+    }
+
+    const { template, metadata } = await transform(str);
+    await fs.writeFile(outputPath, template);
+
 
     if (type in types) {
-      types[type].push({ url, meta: metadata })
+      types[type].push({ url, meta: metadata });
     } else {
-      types[type] = [{ url,  meta: metadata}]
+      types[type] = [{ url, meta: metadata }];
     }
   }
 
-  console.log('generated all needed files. Generating landing/listing pages')
+  console.log("generated all needed files. Generating landing/listing pages");
 
   for (const [type, list] of Object.entries(types)) {
     if (landingPageTemplates[type]) {
-      await fs.writeFile(`${rootDir}/app/${type}/index.html`, landingPageTemplates[type](list))
+      await fs.writeFile(
+        `${rootDir}/app/${type}/index.html`,
+        landingPageTemplates[type](list)
+      );
     } else {
-      console.warn('Cannot create list type for ', type, ' as you never gave me a transformer')
+      console.warn(
+        "Cannot create list type for ",
+        type,
+        " as you never gave me a transformer"
+      );
     }
   }
 
-  console.log('generated all need listing pages.')
-}
+  console.log("generated all need listing pages.");
+};
 
-main()
+main();
