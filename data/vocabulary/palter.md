1470f0babf47d99fde86f1323fe9e5cebd1eb621
Tim Roberts <timiroberts@gmail.com>
2021-11-17 10:40:05 -0500

    Add Vocabulary

---
+++ data/vocabulary/palter.md
@@ -0,0 +1,20 @@
+---
+title: Palter
+tags:
+  - archaic
+synonyms:
+  - fibbing
+  - lying
+  - fabrication
+  - misrepresent
+  - deceive
+  - equivocate
+---
+
+## Palter
+
+> intr.v. pal·tered, pal·ter·ing, pal·ters Archaic
+
+1. To talk or act insincerely or misleadingly; equivocate.
+2. To be capricious; trifle.
+3. To quibble, especially in bargaining.
---
+++ types/vocabulary.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+const rootTemplate = require('../transformers/type-into-list-html')
+const leafTemplate = require('../transformers/markdown-to-html')
+const intoType = require('../transformers/config-into-type')
+
+/**
+ * All actions and paths are based off the
+ * top level folder path to make it easy to grok
+ */
+const rootDir = path.resolve(__dirname, '..')
+
+/**
+ * Our specific Type has its own path as well
+ */
+const typeFolder = path.resolve(rootDir, 'data', 'vocabulary')
+
+/**
+ * The configuration of the Type that we are constructing
+ * 
+ * @prop {string} folder The path to the folder for this Type
+ * @prop {string} rootPath The path that we want to be served under
+ * @prop {{root: function(config): string, leaf: function(config): string }}
+ */
+const config = {
+  folder: typeFolder,
+  rootPath: '/vocabulary',
+  templates: {
+    root: rootTemplate,
+    leaf: leafTemplate
+  },
+  name: 'Vocabulary'
+}
+
+module.exports = intoType(config)
