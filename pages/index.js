cbc223ab0c008328c596ffdfa2d6f74d49c1a7ea
Tim Roberts <timiroberts@gmail.com>
2021-11-20 17:15:22 -0500

    Add RSS Support

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+/**
+ * Custom pages are 1-off writes that we
+ * want to make, given all of the parsed
+ * Pages that the system has. They can
+ * do _anything_ as long as they return a
+ * Promise and cause _some sort_ of HTML
+ * to be written to disk.
+ */
 module.exports.Home = require("./home");
 module.exports.Sitemap = require("./sitemap");
+module.exports.RSS = require('./rss')
+
+/**
+ * This is used to build the generic
+ * pages from data sources. Updating
+ * these will update _everything_. If
+ * you want to update how specific _types_
+ * are rendered, go to their types/<type>
+ * folder and modify the template.
+ */
 module.exports.Internal = {
   Leaf: require("./_leaf"),
   List: require("./_list"),
---
+++ pages/rss.js
@@ -0,0 +1,60 @@
+const trace = require('../utils/trace')
+const config = require('../config/static')
+const fs = require('fs/promises')
+const RSS = require('rss')
+
+const getOrderedPages = (types) => {
+  const listOfPages = []
+  
+  for (const [type, pages] of types.entries()) {
+    for (const page of pages.values()) {
+      listOfPages.push(page)
+    }
+  }
+
+  const orderedByMetadata = listOfPages.sort((a, b) => {
+    if (a.metadata.updated_timestamp > b.metadata.updated_timestamp) {
+      return -1
+    }
+
+    if (a.metadata.updated_timestamp < b.metadata.updated_timestamp) {
+      return 1
+    }
+
+    return 0
+  })
+
+  return orderedByMetadata
+}
+const writeRSSPages = trace(async (types, tags) => {
+  // console.log(types, tags)
+  console.log('We only want to write a feed of ~20  items')
+  const MAX_ITEMS = 20
+  console.log('And we only want the _latest_ 20.')
+  const orderdList = getOrderedPages(types, tags).slice(0, MAX_ITEMS)
+
+  const feed = new RSS({
+    title: 'Tim on a Path',
+    description: 'The RSS Feed for the Internet Portal of Tim Roberts',
+    feed_url: 'https://timonapath.com/rss.xml',
+    site_url: 'https://timonapath.com'
+  })
+
+
+  for (const item of orderdList) {
+    feed.item({
+      title: item.title,
+      url: `https://timonapath.com${item.url}`,
+      categories: item.tags,
+      date: item.updated_timestamp,
+      description: item.summary
+    })
+  }
+
+  await fs.writeFile(`${config.directories.output}/rss.xml`, feed.xml({ indent: true }))
+}, 'Write RSS Pages')
+
+module.exports = {
+  url: '/rss',
+  template: writeRSSPages
+}
